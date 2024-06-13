4ae061c20c1bd8dfdae27657acc85097b77495b0
MaximeS-0 <124843377+Le-pouvoir@users.noreply.github.com>
2024-06-13 14:07:15 -0400

    Create the board game

---
+++ src/board/board.py
@@ -0,0 +1,23 @@
+class board():
+
+    def __init__(self):
+        self._board = [[None]*3 for i in range(3)] 
+
+    def __str__(self):
+        textToShow = ""
+
+        for row in self._board:
+            textToShow += str(row) + "\n"
+        
+        return textToShow
+    
+    @property
+    def boardState(self):
+        return self._board
+    
+    def addValue(self, row:int, column:int, value:str) -> bool:
+        if self._board[row][column] != None:
+            self._board[row][column] = value
+            return True
+        return False
+
--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
+from board.board import board
+
 print("Hello world")
+
+game = board()
+game.addValue(1,0,"x")
+
+print(game.boardState)
+print (game)
