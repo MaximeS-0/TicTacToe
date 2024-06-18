744f68f8dedc97163a1d43dd856b166afc7cceb2
MaximeS-0 <124843377+Le-pouvoir@users.noreply.github.com>
2024-06-18 11:05:22 -0400

    Logic for the game

--- src/Game/board.py
+++ src/Game/board.py
@@ -1,5 +1,10 @@
 class board():
 
+    GAMESTATE_IN_PROGRESS = 0
+    GAMESTATE_PLAYER1_WIN = 1
+    GAMESTATE_PLAYER2_WIN = 2
+    GAMESTATE_TIE = 3
+
     def __init__(self):
         self._board = [[None]*3 for i in range(3)] 
 
@@ -24,3 +29,68 @@ class board():
             return True
         return False
 
+    def gameState(self) -> int:
+        #Check row
+        for row in range(len(self._board)):
+            value = self._board[row][0]
+
+            for column in range(len(self._board[0])):
+                if (self._board[row][column] != value): #If there is a different value, there can't be a winner
+                    break
+                else:  #If it is always the same value, it could be all None, or 1 player has win the game
+                    if value == 1:
+                        return board.GAMESTATE_PLAYER1_WIN
+                    elif value == 2:
+                        return board.GAMESTATE_PLAYER2_WIN
+                    
+                    #If all None, we continue
+    
+        #Check column
+        for column in range(len(self._board[0])):
+            value = self._board[0][column]
+
+            for row in range(len(self._board)):
+                if (self._board[row][column] != value): #If there is a different value, there can't be a winner
+                    break
+                else:  #If it is always the same value, it could be all None, or 1 player has win the game
+                    if value == 1:
+                        return board.GAMESTATE_PLAYER1_WIN
+                    elif value == 2:
+                        return board.GAMESTATE_PLAYER2_WIN
+                    
+                    #If all None, we continue
+
+        #Chek diagonal
+        #First diagonal [(0,0) (1,1) (2,2)]
+        value = self._board[0][0]
+        for i in range(len(self._board)):
+            if (self._board[i][i] != value): #If there is a different value, there can't be a winner
+                    break
+            else:  #If it is always the same value, it could be all None, or 1 player has win the game
+                if value == 1:
+                    return board.GAMESTATE_PLAYER1_WIN
+                elif value == 2:
+                    return board.GAMESTATE_PLAYER2_WIN
+                    
+                    #If all None, we continue
+
+        #Second diagonal [(2,0) (1,1) (0,2)]
+        value = self._board[2][0]
+        for i in range(len(self._board)):
+            if (self._board[i][len(self._board[0])-1 - i] != value): #If there is a different value, there can't be a winner
+                    break
+            else:  #If it is always the same value, it could be all None, or 1 player has win the game
+                if value == 1:
+                    return board.GAMESTATE_PLAYER1_WIN
+                elif value == 2:
+                    return board.GAMESTATE_PLAYER2_WIN
+                    
+                    #If all None, we continue
+
+        #Check tie
+        for row in range(len(self._board)):
+            for column in range(len(self._board[0])):
+                if self._board[row][column] == None:
+                    return board.GAMESTATE_IN_PROGRESS
+        
+        return board.GAMESTATE_TIE
--- src/Game/game.py
+++ src/Game/game.py
@@ -1,5 +1,5 @@
-import board
-import player
+from board import board
+from player import player
 
 class game():
     def __init__(self):
@@ -10,11 +10,13 @@ class game():
         self.playerList = [self.player1, self.player2]
 
     def playTurn(self):
-        self._playMove()
+        self._playMove(self.player1)
         self._isGameCompleted()
 
-    def _playMove(self):
-        pass
 
+    def _playMove(self, activePlayer: player):
+        (row, col) = activePlayer.makeMove()
+        
     def _isGameCompleted(self):
         pass
+
